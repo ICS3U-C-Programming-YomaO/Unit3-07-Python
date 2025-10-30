f7f9855cd6950f6b3115bafd8931b9ec81b3f696
Yoma Ozoh <ogeneyoma.ozoh@stu.ocsb.ca>
2025-10-30 17:50:09 +0000

    tinder

---
+++ dating.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# Created By: Yoma Ozoh
+# Date: October 2025
+# This program generates a random number between 0 and 9
+# and checks if the user guessed it correctly
+
+import random
+
+
+def main():
+
+    # get age from user
+    user_age = input("Hey! Enter your age please: ")
+    # type cast to integer
+    try:
+        user_age = int(user_age)
+
+        # check if age within requirements 
+        if user_age > 25 and user_age < 41:
+            print("You can date my grandchild!")
+        else:
+            print("You cannot date my grandchild")
+
+    except ValueError:
+        # exception handling
+        print("ERROR! You have to enter a valid age!")
+    #end game
+    finally:
+        print("Thanks for playing my game!")
+
+
+if __name__ == "__main__":
+    main()
